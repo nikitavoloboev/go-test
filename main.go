67befbc2749ff755b4cfbd8464b80467667e367d
Nikita <github@nikiv.dev>
2025-04-18 02:30:51 +0200

    .

--- main.go
+++ main.go
@@ -1,15 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"os"
-
-	"github.com/nikitavoloboev/go-test/cmd"
-)
-
 func main() {
-	if err := cmd.RootCmd.Execute(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
 }
